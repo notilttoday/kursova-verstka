e983729468b689c98c1a785dbfcc9a6108b8208c
‘notilttoday’ <tsimbaloleg2006@gmail.com>
2024-10-21 01:03:14 +0300

    third commit

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const profileDropdown = document.querySelector('.profile-dropdown');
-    const dropdownList = document.querySelector('.dropdown-list');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const profileDropdown = document.querySelector('.profile-dropdown');
+//     const dropdownList = document.querySelector('.dropdown-list');
 
-    profileDropdown.addEventListener('mouseenter', () => {
-        dropdownList.style.display = 'block';
-    });
+//     profileDropdown.addEventListener('mouseenter', () => {
+//         dropdownList.style.display = 'block';
+//     });
 
-    profileDropdown.addEventListener('mouseleave', () => {
-        dropdownList.style.display = 'none';
-    });
-});
+//     profileDropdown.addEventListener('mouseleave', () => {
+//         dropdownList.style.display = 'none';
+//     });
+// });
+
+function increaseQuantity() {
+    const input = document.getElementById('quantity');
+    let currentValue = parseInt(input.value);
+    input.value = currentValue + 1;
+    updateTotalPrice();
+}
+
+function decreaseQuantity() {
+    const input = document.getElementById('quantity');
+    let currentValue = parseInt(input.value);
+    if (currentValue > 1) {
+        input.value = currentValue - 1;
+    }
+    updateTotalPrice();
+}
+
+// function updateTotalPrice() {
+//     const input = document.getElementById('quantity');
+//     const quantity = parseInt(input.value);
+//     const totalPriceElement = document.getElementById('price');
+//     const totalPrice = unitPrice * quantity;
+//     totalPriceElement.textContent = totalPrice;
+// }
